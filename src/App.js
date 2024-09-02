e6b0047c94ea2f7f1ad0ea99259e85c73173612e
Fatemeh <fatemeh.rad92@gmail.com>
2024-09-02 14:27:59 +0200

    different components added with their classee

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 12, packed: false },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return (
+    <div>
+      <h1>🌴Far Away🧳</h1>
+    </div>
+  );
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for you 😍 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <opion value={num} key={num}>
+            {num}
+          </opion>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item obj={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ obj }) {
+  return (
+    <li>
+      <span style={obj.packed ? { textDecoration: "Line-through" } : {}}>
+        {obj.quantity}
+        {obj.description}{" "}
+      </span>
+      <button>❌&times</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>you have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
+
